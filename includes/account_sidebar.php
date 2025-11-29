bf5f8013d1e888b4601daefd84924b5233c542aa
csomuah3 <144915674+csomuah3@users.noreply.github.com>
2025-11-29 17:51:24 +0000

    Update help center link to point to contact.php instead of help_center.php

--- includes/account_sidebar.php
+++ includes/account_sidebar.php
@@ -14,7 +14,7 @@
         <li><a href="my_orders.php" class="<?php echo basename($_SERVER['PHP_SELF']) == 'my_orders.php' ? 'active' : ''; ?>"><i class="fas fa-box"></i>My Orders</a></li>
         <li><a href="wishlist.php" class="<?php echo basename($_SERVER['PHP_SELF']) == 'wishlist.php' ? 'active' : ''; ?>"><i class="fas fa-heart"></i>My Wishlist</a></li>
         <li><a href="compare.php" class="<?php echo basename($_SERVER['PHP_SELF']) == 'compare.php' ? 'active' : ''; ?>"><i class="fas fa-balance-scale"></i>Compare</a></li>
-        <li><a href="help_center.php" class="<?php echo basename($_SERVER['PHP_SELF']) == 'help_center.php' ? 'active' : ''; ?>"><i class="fas fa-question-circle"></i>Help Center</a></li>
+        <li><a href="contact.php" class="<?php echo basename($_SERVER['PHP_SELF']) == 'contact.php' ? 'active' : ''; ?>"><i class="fas fa-question-circle"></i>Help Center</a></li>
         <li><a href="../login/logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a></li>
     </ul>
 </aside>
